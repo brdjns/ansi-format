2a2fe8f61c1651ebc10feccf2b6651f5a31772fb
Bradley Jones <brdjns@gmx.us>
2025-01-17 13:15:59 +1300

    Make work with personal comment style

--- tests/Main.cc
+++ tests/Main.cc
@@ -3,7 +3,15 @@
 // this is a modern C++ comment
 // infact, this is a block
 
+
+// Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam nec
+// libero nec libero. Nullam nec libero nec libero. Nullam nec libero nec
+// libero. Nullam nec libero nec libero. Nullam nec libero nec libero.
+// Nullam nec libero nec libero. Nullam nec libero nec libero. Nullam nec
+
 int main() {
+    // Lorem ipsum sit amet, consectetur
+    // adipiscing elit. Nullem nec libero.
     return 0;
 }
 
